47f70e4b902d4963ffb24c29916b16b6c74a5af1
Thiago <thiago.andrade.devs@gmail.com>
2021-12-02 17:28:40 -0300

    gerenciador

--- src/gerenciador-tarefas.js
+++ src/gerenciador-tarefas.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './gerenciador-tarefas.css';
 
+
 function GerenciadoTarefas() {
   return (
     <h1>Gerenciador de Tarefas</h1>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import GerenciadoTarefas from './gerenciador-tarefas';
 
+
 ReactDOM.render(
   <GerenciadoTarefas />,
   document.getElementById('root')
