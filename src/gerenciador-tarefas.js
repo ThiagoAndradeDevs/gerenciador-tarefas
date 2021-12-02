17851aa6bcb2b1c90a84579f26a08e915cf1b550
Thiago <thiago.andrade.devs@gmail.com>
2021-12-02 17:16:03 -0300

    gerenciador

---
+++ src/gerenciador-tarefas.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './gerenciador-tarefas.css';
+
+function GerenciadoTarefas() {
+  return (
+    <h1>Gerenciador de Tarefas</h1>
+  );
+}
+
+export default GerenciadoTarefas;
