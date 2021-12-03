f23c292b27216ed363b3f9781ac261ae9e1b174f
Thiago <thiago.andrade.devs@gmail.com>
2021-12-03 13:43:04 -0300

    test

--- src/gerenciador-tarefas.test.js
+++ src/gerenciador-tarefas.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import GerenciadoTarefas from './gerenciador-tarefas';
 
-test('renders learn react link', () => {
+test('deve reinderizar projeto sem erros', () => {
   render(<GerenciadoTarefas />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
