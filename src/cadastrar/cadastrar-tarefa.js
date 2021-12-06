290a68daf62a05b0da1542b6d6f57d9e86ea4e66
Thiago <thiago.andrade.devs@gmail.com>
2021-12-06 15:25:40 -0300

    atualizar

--- src/cadastrar/cadastrar-tarefa.js
+++ src/cadastrar/cadastrar-tarefa.js
@@ -1,8 +1,13 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+
+
 
 function CadastrarTarefa() {
   return (
-    <div>Cadastrar Tarefa!!</div>
+    <div>
+      <h3 className='text-center'>Cadastar</h3>
+    </div>
   )
 }
 export default CadastrarTarefa;
