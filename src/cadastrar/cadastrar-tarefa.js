f5566f65f101dd81611c8be7b216113e099b844a
Thiago <thiago.andrade.devs@gmail.com>
2021-12-06 17:38:26 -0300

    form

--- src/cadastrar/cadastrar-tarefa.js
+++ src/cadastrar/cadastrar-tarefa.js
@@ -1,12 +1,38 @@
 /* eslint-disable no-unused-vars */
+import { A } from 'hookrouter';
 import React, { useState } from 'react';
-import { Button, Form, Jumbotron, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap'
 
 
 function CadastrarTarefa() {
   return (
     <div>
       <h3 className='text-center'>Cadastar</h3>
+      <div className='Jumbo'>
+        <Form>
+          <Form.Group>
+            <Form.Label>Tarefa</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Digitar a tarefa"
+              minLength="5"
+              maxLength="100"
+              required></Form.Control>
+            <Form.Control.Feedback type="invalid">
+              A tarefa deve conter ao menos 5 caracteres.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group>
+            <Button
+              variant="success"
+              type="submit">
+              Cadastrar
+            </Button>
+            &nbsp;
+            <A href="/" className="btn btn-light">Voltar</A>
+          </Form.Group>
+        </Form>
+      </div>
 
     </div>
   )
