e6e742bb7e4859ce9be408b65787193ca230192a
Thiago <thiago.andrade.devs@gmail.com>
2021-12-07 16:05:20 -0300

    testing-library

--- src/cadastrar/cadastrar-tarefa.test.js
+++ src/cadastrar/cadastrar-tarefa.test.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CadastrarTarefa from './cadastrar-tarefa';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 
 
 describe('Teste do componente de cadastro de tarefas', () => {
@@ -9,5 +11,8 @@ describe('Teste do componente de cadastro de tarefas', () => {
     const div = document.createElement('div');
     ReactDOM.render(<CadastrarTarefa />, div);
     ReactDOM.unmountComponentAtNode(div);
+  })
+  it('Deve cadastrar uma nova tarefa', () => {
+    const { getByTestId } = render(<CadastrarTarefa />);
   })
 })
