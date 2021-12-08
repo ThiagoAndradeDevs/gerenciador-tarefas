487e0a2b7da6405d64e7695fe9353f2a0d1bda34
Thiago <thiago.andrade.devs@gmail.com>
2021-12-08 16:50:19 -0300

    icon

--- src/listar/itens-lista-tarefas.js
+++ src/listar/itens-lista-tarefas.js
@@ -1,9 +1,31 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { A } from 'hookrouter';
 function ItensListaTarefas(props) {
 
   return (
-    <div></div>
+    props.tarefas.map(tarefa =>
+      <tr key={tarefa.id} data-testid="tarefa">
+        <td width="75%"
+          data-testid="nome-tarefa">
+          {tarefa.nome}
+        </td>
+        <td className="text-right">
+          <A href={"/atualizar/" + tarefa.id}
+            className={tarefa.concluida ? 'hidden' : 'btn btn-warning btn-sm'}>
+            <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+          </A>
+        </td>
+      </tr>
+    )
   )
 };
+ItensListaTarefas.propTypes = {
+  tarefas: PropTypes.array.isRequired,
+  recarregarTarefas: PropTypes.func.isRequired
+}
 
 export default ItensListaTarefas;
