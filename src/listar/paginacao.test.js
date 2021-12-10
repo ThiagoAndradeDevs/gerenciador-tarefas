9c21e4178b7b11dcb3caeecaefd8bdbe2b186881
Thiago <thiago.andrade.devs@gmail.com>
2021-12-10 16:15:13 -0300

    test

--- src/listar/paginacao.test.js
+++ src/listar/paginacao.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Paginacao from './paginacao';
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 
 
@@ -16,6 +16,19 @@ describe('Teste do componente de paginaçao', () => {
         paginaAtual={1}
         mudarPagina={() => false} />, div);
     ReactDOM.unmountComponentAtNode(div);
+  })
+  it('deve exibir a paginaçao contendo 3 paginas', () => {
+    const { getByTestId } = render(
+      <Paginacao
+        totalItens={15}
+        itensPorPagina={5}
+        paginaAtual={1}
+        mudarPagina={() => false} />
+    )
+    const paginacao = getByTestId('paginacao');
+    expect(paginacao).toHaveTextContent('1');
+    expect(paginacao).toHaveTextContent('2');
+    expect(paginacao).toHaveTextContent('3');
   })
 
 })
