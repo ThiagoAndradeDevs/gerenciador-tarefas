1ba5b498eb12092cc6adebaabe69b20df33ed830
Thiago <thiago.andrade.devs@gmail.com>
2021-12-13 14:53:10 -0300

    test atualizar

--- src/atualizar/atualizar-tarefas.test.js
+++ src/atualizar/atualizar-tarefas.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,6 +6,7 @@ import AtualizarTarefa from './atualizar-tarefas';
 import Tarefa from "../models/tarefa.model";
 import { render, FireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { A } from 'hookrouter';
 
 
 describe('Teste do componente de atualizaçao de tarefas', () => {
@@ -18,5 +20,18 @@ describe('Teste do componente de atualizaçao de tarefas', () => {
     const div = document.createElement('div');
     ReactDOM.render(<AtualizarTarefa id={tarefaId} />, div);
     ReactDOM.unmountComponentAtNode(div);
+  })
+  it('deve exibir a modal de sucesso ao atualizar uma tarefa', () => {
+    const { getByTestId } = render(<AtualizarTarefa id={tarefaId} />);
+    fireEvent.cliclk(getByTestId('btn-atualizar'));
+    expect(getByTestId('modal')).toHaveTextContent('Sucesso');
+  });
+  it('deve atualizar a tarefa', () => {
+    const nomeTarefaAtualizada = 'Tarefa aualizada';
+    const { getByTestId } = render(<AtualizarTarefa id={tarefaId} />);
+    fireEvent.change(getByTestId(txt - tarefa), { target: { value: nomeTarefaAtualizada } })
+    fireEvent.cliclk(getByTestId('btn-atualizar'));
+    const tarefasDb = JSON.parse(localStorage['tarefas']);
+    expect(tarefasDb[0].nome).toBe(nomeTarefaAtualizada);
   })
 })
